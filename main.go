096d463434a1aae93a7ea23c85251e3ef22e6486
Kostas Christidis <kostas@christidis.io>
2017-11-19 11:41:21 -0500

    Record segment count for points

--- main.go
+++ main.go
@@ -6,15 +6,24 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"sort"
 	"strconv"
 	"strings"
 )
 
 type id string
 
-type interval struct {
-	start, end float64
+type segment struct {
+	start, end int64
 }
+
+// Implement sort.Interface
+// https://golang.org/pkg/sort/#Interface
+type int64arr []int64
+
+func (a int64arr) Len() int           { return len(a) }
+func (a int64arr) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a int64arr) Less(i, j int) bool { return a[i] < a[j] }
 
 func main() {
 	// Read a file with tuples [id, start, end]
@@ -27,8 +36,9 @@ func main() {
 	r := bytes.NewReader(b)
 	scanner := bufio.NewScanner(r)
 	var line string
-	var elem interval
-	table := make(map[id]interval)
+	var s segment
+	var start, end float64
+	segments := make(map[id]segment)
 	for scanner.Scan() {
 		if err := scanner.Err(); err != nil {
 			panic(err)
@@ -42,22 +52,42 @@ func main() {
 		}
 		// The line has 3 fields as expected
 		// Let's convert to the appropriate types
-		elem.start, err = strconv.ParseFloat(vals[1], 64)
+		start, err = strconv.ParseFloat(vals[1], 64)
 		if err != nil {
 			panic(err)
 		}
-		elem.end, err = strconv.ParseFloat(vals[2], 64)
+		s.start = int64(start)
+		end, err = strconv.ParseFloat(vals[2], 64)
 		if err != nil {
 			panic(err)
 		}
-		table[id(vals[0])] = elem
+		s.end = int64(end)
+		segments[id(vals[0])] = s
+	}
+
+	// Concatenate the start and end points
+	var points int64arr
+	for _, v := range segments {
+		points = append(points, v.start, v.end)
+	}
+	// Sort them in ascending order
+	sort.Sort(points)
+	// Record the counts for each point
+	var counts []int
+	var count int
+	for _, p := range points {
+		count = 0
+		// Given a point, let's figure out the segments it belongs to
+		for _, s := range segments {
+			if p >= s.start && p <= s.end {
+				count++
+			}
+		}
+		counts = append(counts, count)
 	}
 
 	// Debug
-	var count int
-	for k := range table {
-		count++
-		fmt.Println(k)
+	for i := 0; i < len(points); i++ {
+		fmt.Printf("[%d] %d\n", points[i], counts[i])
 	}
-	fmt.Printf("Number of lines: %d\n", count)
 }
