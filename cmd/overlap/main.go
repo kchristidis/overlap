cfd2941d16f293c6d40c54da08dd013266e396f5
Kostas Christidis <kostas@christidis.io>
2017-11-24 20:56:06 -0500

    Split between library and binary

---
+++ cmd/overlap/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/kchristidis/overlap"
+)
+
+func main() {
+	// Use the library
+	results, err := overlap.Calculate("unix_tuples.txt")
+	if err != nil {
+		panic(err)
+	}
+	// Write results to file
+	f, err := os.Create("results.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	for _, v := range results {
+		fmt.Fprintf(w, "%.2f\t%0.f\t%0.f\t%d\t%v\n",
+			v.OverlapLength/(60*60*8760),
+			v.OverlapStart, v.OverlapEnd,
+			v.SegmentCount, v.SegmentList)
+	}
+	w.Flush()
+}
--- overlap.go
+++ overlap.go
@@ -1,24 +1,14 @@
-package main
+package overlap
 
 import (
 	"bufio"
 	"bytes"
-	"errors"
 	"fmt"
 	"io/ioutil"
-	"os"
 	"sort"
 	"strconv"
 	"strings"
-	"time"
-
-	"go.uber.org/zap"
 )
-
-type segmentImpl struct {
-	id         int
-	start, end float64
-}
 
 type pointImpl struct {
 	loc float64
@@ -39,61 +29,77 @@ func (p *pointImpl) belongsTo() []int {
 	return p.in
 }
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
+type segmentImpl struct {
+	id         int
+	start, end float64
 }
 
-func main() {
-	// Create a logger
-	logger, err := zap.NewProduction()
-	check(err)
-	defer logger.Sync()
-	sugar := logger.Sugar()
+// Result describes an overlap. It carries its length, its start
+// and end points, the number of segments that go over it, as well
+// as their IDs.
+type Result struct {
+	OverlapLength            float64
+	OverlapStart, OverlapEnd float64
+	SegmentCount             int
+	SegmentList              []int
+}
 
+// Calculate reads a file with segments and returns a slice of their overlaps.
+// Each line (segment) in the input file should follow the format:
+//		segment_id(int) segment_start(float64) segment_end(float64)
+// The fields are tab-separated. Their types are listed in parentheses.
+func Calculate(filePath string) ([]Result, error) {
 	// Load a file with tuples [id, start, end]
-	b, err := ioutil.ReadFile("unix_tuples.txt")
-	check(err)
+	b, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("could not read file %s: %s", filePath, err)
+	}
 
 	// Read file into slice
 	r := bytes.NewReader(b)
 	scanner := bufio.NewScanner(r)
+	var count int
 	var line string
 	var segment segmentImpl
 	var segments []segmentImpl
 	for scanner.Scan() {
-		err = scanner.Err()
-		check(err)
+		if err := scanner.Err(); err != nil {
+			return nil, err
+		}
 		line = scanner.Text()
 		// We now have a line
+		count++
 		// Does it have 3 fields?
 		vals := strings.Split(line, "\t")
 		if len(vals) != 3 {
-			panic(errors.New("Expected 3 fields per line"))
+			return nil, fmt.Errorf("expected 3 fields per line, got %d instead", len(vals))
 		}
 		// The line has 3 fields as expected
-		// Let's convert to the appropriate types
+		// Convert to the appropriate types
 		segment.id, err = strconv.Atoi(vals[0])
-		check(err)
+		if err != nil {
+			return nil, fmt.Errorf("could not convert element %v in column 1, row %d to an integer: %s", vals[0], count, err)
+		}
 		segment.start, err = strconv.ParseFloat(vals[1], 64)
-		check(err)
+		if err != nil {
+			return nil, fmt.Errorf("could not convert element %v in column 2, row %d to a floating-point number: %s", vals[1], count, err)
+		}
 		segment.end, err = strconv.ParseFloat(vals[2], 64)
-		check(err)
+		if err != nil {
+			return nil, fmt.Errorf("could not convert element %v in column 3, row %d to a floating-point number: %s", vals[2], count, err)
+		}
 		segments = append(segments, segment)
 	}
-	sugar.Debug("Number of segments in file: ", len(segments))
 
 	// Create list of unique points
-	// Use a map so as to remove duplicates
+	// Use a map to remove duplicates
 	locMap := make(map[float64]bool)
 	for _, v := range segments {
 		locMap[v.start] = true
 		locMap[v.end] = true
 	}
-	sugar.Debug("Number of unique points: ", len(locMap))
 
-	// Turn into a slice so that you can sort it in ascending order
+	// Turn into slice so that you can sort in ascending order
 	var locList []float64
 	for k := range locMap {
 		locList = append(locList, k)
@@ -101,7 +107,7 @@ func main() {
 	// Sort list in ascending order
 	sort.Float64s(locList)
 
-	// For each point, record the segments that it belongs to
+	// Recrod the segments that each point belongs to
 	var points []*pointImpl
 	for _, loc := range locList {
 		p := &pointImpl{loc: loc}
@@ -117,20 +123,18 @@ func main() {
 
 	// `locList` carries the monotonically increasing list of unique points
 	// `points` carries the same list, plus the segments that each point corresponds to
-	// `result` includes the data we're interested in
-	result := make(map[float64]map[float64][]int)
+	// `resMap` includes the data we're interested in
+	resMap := make(map[float64]map[float64][]int)
 	for i := 0; i < len(points)-1; i++ {
 		// Initialize the inner map
 		// See: https://stackoverflow.com/a/44305711/2363529
-		result[points[i].loc] = make(map[float64][]int)
+		resMap[points[i].loc] = make(map[float64][]int)
 		// What are the segments that go over this point?
 		segStart := points[i].belongsTo()
-		sugar.Debugf("Examining point %0.f which belongs to segments: %v\n", points[i].loc, segStart)
 		for j := len(points) - 1; j > i; j-- {
 			var overlap []int
 			// What are the segments that go over this point?
 			segEnd := points[j].belongsTo()
-			sugar.Debugf("\tComparing with point %0.f which belongs to segments: %v\n", points[j].loc, segEnd)
 			// What is the overlap between segStart and segEnd?
 			for _, seg1 := range segStart {
 				for _, seg2 := range segEnd {
@@ -141,30 +145,28 @@ func main() {
 				}
 			}
 			if len(overlap) > 1 {
-				result[points[i].loc][points[j].loc] = overlap
-				sugar.Debugf("\t\tThe two points overlap in: %v\n", overlap)
-			} else {
-				sugar.Debugf("\t\tThe two points do not overlap\n")
+				resMap[points[i].loc][points[j].loc] = overlap
 			}
 			// Now let's examine the next segEnd (if any)
 		}
 	}
 
-	// Write results to file
-	f, err := os.Create("results.txt")
-	check(err)
-	defer f.Close()
-	w := bufio.NewWriter(f)
-	for k1, v1 := range result {
+	// Return
+	var results []Result
+	for k1, v1 := range resMap {
 		for k2, v2 := range v1 {
 			// Line format:
 			// overlap length (years) - overlap start - overlap end - segment count - segment list
-			_, err := fmt.Fprintf(w, "%.2f\t%0.f\t%0.f\t%d\t%v\n", (k2-k1)/(60*60*8760), k1, k2, len(v2), v2)
-			check(err)
-			// Print to screen
-			sugar.Debugf("Overlap starting from %0.f (%s) and ending at %0.f (%s) (length: %.1fd) shared by %d segments: %v\n",
-				k1, time.Unix(int64(k1), 0), k2, time.Unix(int64(k2), 0), (k2-k1)/60/60/24, len(v2), v2)
+			r := Result{
+				OverlapLength: k2 - k1,
+				OverlapStart:  k1,
+				OverlapEnd:    k2,
+				SegmentCount:  len(v2),
+				SegmentList:   v2,
+			}
+			results = append(results, r)
 		}
 	}
-	w.Flush()
+
+	return results, nil
 }
