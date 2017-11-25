9f1ef4ddf34c08ecfd9854b51c551b19fd2059e0
Kostas Christidis <kostas@christidis.io>
2017-11-25 17:07:03 -0500

    Add test for all possible bad lines

--- overlap_test.go
+++ overlap_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -31,11 +32,21 @@ func TestBadLine(t *testing.T) {
 	f, err := ioutil.TempFile("", "good.file")
 	require.Nil(t, err, "could not create temp file")
 	defer os.Remove(f.Name())
-	badLine := []byte("foo\t1\t2") // 2 fields
-	_, err = f.Write(badLine)
+	badLine1 := []byte("foo\t1\t2")
+	_, err = f.Write(badLine1)
 	require.Nil(t, err, "could not write to input file")
 	_, err = Calculate(f.Name())
-	require.Error(t, err, "expected error")
+	assert.Error(t, err, "expected error")
+	badLine2 := []byte("0\tfoo\t2")
+	_, err = f.WriteAt(badLine2, 0)
+	require.Nil(t, err, "could not write to input file")
+	_, err = Calculate(f.Name())
+	assert.Error(t, err, "expected error")
+	badLine3 := []byte("0\t1\tfoo")
+	_, err = f.WriteAt(badLine3, 0)
+	require.Nil(t, err, "could not write to input file")
+	_, err = Calculate(f.Name())
+	assert.Error(t, err, "expected error")
 }
 
 func TestPoint(t *testing.T) {
