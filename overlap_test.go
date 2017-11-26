cd8cab7f6b19fd3897dc9920b6490f79e276874b
Kostas Christidis <kostas@christidis.io>
2017-11-25 20:28:55 -0500

    Add calculate test

--- overlap_test.go
+++ overlap_test.go
@@ -29,14 +29,34 @@ func TestBadLines(t *testing.T) {
 	require.Nil(t, err, "could not create temp file")
 	defer os.Remove(f.Name())
 	lines := make([][]byte, 4)
-	lines[0] = []byte("1\t0")
-	lines[1] = []byte("foo\t1\t2")
-	lines[2] = []byte("0\tfoo\t2")
-	lines[3] = []byte("0\t1\tfoo")
-	for i := 0; i < len(lines); i++ {
+	lines[0] = []byte("0\t100")
+	lines[1] = []byte("foo\t100\t200")
+	lines[2] = []byte("0\tfoo\t200")
+	lines[3] = []byte("0\t100\tfoo")
+	for i := range lines { // https://stackoverflow.com/a/39806983/2363529
 		_, err = f.WriteAt(lines[i], 0)
 		require.Nil(t, err, "could not write to input file")
 		_, err = Calculate(f.Name())
 		require.Error(t, err, "expected error")
 	}
+}
+
+func TestCalculate(t *testing.T) {
+	f, err := ioutil.TempFile("", "good.file")
+	require.Nil(t, err, "could not create temp file")
+	defer os.Remove(f.Name())
+	// https://stackoverflow.com/a/39806983/2363529
+	lines := []byte("0\t50.0\t150.0" + "\n" + "1\t100\t200.0")
+	_, err = f.Write(lines)
+	require.Nil(t, err, "could not write to input file")
+	res, err := Calculate(f.Name())
+	require.Nil(t, err, "expected no error")
+	require.Equal(t, len(res), 1, "expected 1 overlap")
+	require.Equal(t, res[0], Result{
+		OverlapLength: float64(50),
+		OverlapStart:  float64(100),
+		OverlapEnd:    float64(150),
+		SegmentCount:  2,
+		SegmentList:   []int{0, 1},
+	}, "expected [100,150] overlap between the segments")
 }
